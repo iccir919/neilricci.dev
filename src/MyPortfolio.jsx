e55b1ca0c3dc04a0f1d7d4c2c6c8ae4dc25ff797
Neil <iccir919@gmail.com>
2025-09-13 12:15:35 -0700

    first commit!

---
+++ src/MyPortfolio.jsx
@@ -0,0 +1,13 @@
+function MyPortfolio() {
+
+  return (
+    <main>
+      <div class="text-container">
+        <h1>neilricci.dev</h1>
+        <p>coming soon</p>
+      </div>
+    </main>
+  )
+}
+
+export default MyPortfolio
