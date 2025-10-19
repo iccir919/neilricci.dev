e99efe1247b714df0fbcf837a13de1463fd95beb
Neil <iccir919@gmail.com>
2025-10-18 22:38:59 -0700

    Update styling of Projects page

--- projects.js
+++ projects.js
@@ -20,18 +20,16 @@ projects.forEach(project => {
         .join(" ")
 
     projectElement.innerHTML = `
-        <h2>${project.name}</h2>
-        <p class="description">${project.description}</p>
-        <p><strong>Date completed:</strong> ${formattedDate}</p>
         <img src="${project.imageUrl}" alt="Screenshot of ${project.name}" />
-        <div class="skills">${skills}</div>
-        <div class="links">
-            <a href="${project.github_repo}" target="_blank">
-                Github
-            </a>
-            <a href="${project.link}" target="_blank" class="primary">
-                Live
-            </a>
+        <div class="project-content">
+            <h2>${project.name}</h2>
+            <p class="description">${project.description}</p>
+            <p><strong>Date completed:</strong> ${formattedDate}</p>
+            <div class="skills">${skills}</div>
+            <div class="links">
+                <a href="${project.github_repo}" target="_blank">GitHub</a>
+                <a href="${project.link}" target="_blank" class="primary">Live</a>
+            </div>
         </div>
     `
 
